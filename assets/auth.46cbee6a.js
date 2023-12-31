2d9cd036de86da00a01350dbb56e080fc70d8e3e
Nasimul Hasan <nasimulhasandeep@gmail.com>
2024-01-01 00:26:41 +0600

    Updates

--- assets/auth.46cbee6a.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./EngineeringLogo.67df3d14.js";import{_ as o,v as c,x as r,y as e,A as a,b2 as _}from"./entry.57377fff.js";const i={},d={class:"py-10 min-h-screen bg_image"},l={class:"flex items-center justify-center"};function m(t,f){const s=n;return c(),r("div",d,[e("div",null,[e("div",l,[a(s)])]),_(t.$slots,"default")])}const g=o(i,[["render",m]]);export{g as default};
--- assets/auth.6cbe971c.js
+++
@@ -1,1 +0,0 @@
-import{bX as s,bY as t,c as o,s as a}from"./entry.57377fff.js";const i=s((c,n)=>{const e=t();if(o(()=>e.access_token).value===null)return a("/login")});export{i as default};
